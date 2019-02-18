daa2bd33f22c35adf4f10685ae41e3b3679490f9
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-18 15:32:29 +0100

    ajout des activités

--- app/src/main/java/fr/isen/firstpackage/gothamcasino/WallActivity.kt
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/WallActivity.kt
@@ -3,10 +3,10 @@ package fr.isen.firstpackage.gothamcasino
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WallActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_wall)
     }
 }

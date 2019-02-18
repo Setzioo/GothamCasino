0bd32e132136ca8373da1601dceee5542f2c5f3c
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-02-18 17:28:40 +0100

    Ajout des classes modeles : UserModel, ArticleModel et CommentModel

---
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/CommentModel.kt
@@ -0,0 +1,7 @@
+package fr.isen.firstpackage.gothamcasino
+
+data class CommentModel(
+    val articleId: Int,
+    val userId: Int,
+    val content: String
+)
---
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/UserModel.kt
@@ -0,0 +1,8 @@
+package fr.isen.firstpackage.gothamcasino
+
+data class UserModel(
+    val id: Int,
+    val lastName: String,
+    val firstName: String,
+    val avatar: String
+)

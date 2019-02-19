f5bcc382171ac59bb25b1682df41f9eee691d9b6
Alexis <alexis.valentin-ranc@isen.yncrea.fr>
2019-02-19 11:22:37 +0100

    import login

--- app/src/main/java/fr/isen/firstpackage/gothamcasino/LoginActivity.kt
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/LoginActivity.kt
@@ -2,6 +2,15 @@ package fr.isen.firstpackage.gothamcasino
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import kotlinx.android.synthetic.main.activity_post.*
+import java.util.*
+import android.content.Intent
+import android.app.Activity
+import android.util.Log
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+
 
 class LoginActivity : AppCompatActivity() {
 

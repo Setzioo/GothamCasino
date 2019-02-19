3789b23a7fc76d0b66e5bcb782cf91aac0b8f5aa
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-02-19 16:23:05 +0100

    Merge branch 'master' of C:\Users\setzi\AndroidStudioProjects\GothamCasino with conflicts.

--- app/src/main/java/fr/isen/firstpackage/gothamcasino/PostActivity.kt
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/PostActivity.kt
@@ -1,7 +1,9 @@
 package fr.isen.firstpackage.gothamcasino
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_post.*
 
@@ -14,6 +16,11 @@ class PostActivity : AppCompatActivity() {
             onPublish()
         }
 
+        returnButton.setOnClickListener{
+            val intent = Intent(this, WallActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
 
@@ -21,6 +28,24 @@ class PostActivity : AppCompatActivity() {
         var commentText: String = inputComment.text.toString()
         var titleText: String = inputText.text.toString()
         Toast.makeText(this, commentText, Toast.LENGTH_LONG).show()
+
+        val article = HashMap<String, Any>()
+        article["userId"] = "myId"
+        article["title"] = titleText
+        article["description"] = commentText
+        article["photoUrl"] = ""
+        article["nbLike"] = 0
+        article["nbDislike"] = 0
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("ArticleBase")
+            .add(article)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot written with ID: " + documentReference.id)
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
     }
 
 }

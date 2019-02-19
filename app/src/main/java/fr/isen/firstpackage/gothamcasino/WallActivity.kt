2908c31828fd2e6dac8777fb3b927014a3cdc3b1
Telly Martel-Audion <telly.martel-audion@isen.yncrea.fr>
2019-02-19 10:00:15 +0100

    Ajout du recyclerView dans WallActivity :
    - CustomAdapter
    - ArticleViewRowFragment(kt et xml)

--- app/src/main/java/fr/isen/firstpackage/gothamcasino/WallActivity.kt
+++ app/src/main/java/fr/isen/firstpackage/gothamcasino/WallActivity.kt
@@ -1,12 +1,31 @@
 package fr.isen.firstpackage.gothamcasino
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_wall.*
 
 class WallActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wall)
+
+        val articles = ArrayList<ArticleModel>()
+        for (i in 0..20) {
+            val tempComments = arrayListOf<CommentModel>()
+            val tempArticle = ArticleModel(i,i,"Titre $i","Description $i","url $i",i*48196452,i*1456, tempComments)
+            // new item
+            articles.add(tempArticle)
+        }
+
+        articleView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+        val adapter = CustomAdapter(articles)
+        articleView.adapter = adapter
     }
+
+
+
 }
